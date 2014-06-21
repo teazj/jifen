94b1d3b69ae0fd592b58ac4b533accd278ab1b9a
daxue <627176023@qq.com>
2014-06-21 21:46:03 +0800

    首页列表页，详情页

--- Home/Lib/Action/GlobalAction.class.php
+++ Home/Lib/Action/GlobalAction.class.php
@@ -1,8 +1,9 @@
 <?php
 class GlobalAction extends Action{
 	Public function _initialize() {
-		import("ORG.Util.Cart");				
-		$this->userID =session('userID');
+		import("ORG.Util.Cart");	
+		$userinfo=session('FEUSER');			
+		$this->userID =$userinfo['id'];
 		$this->cart = new Cart();
 		$this->assign('cartCount', $_SESSION['cart']['total_num']);	
 		$this->assign('cartbox',$_SESSION['cart']['goods_list']);	
--- Public/Home/js/function.js
+++ Public/Home/js/function.js
@@ -1,23 +1,23 @@
 
 
-					$(function(){
-        			var default_key = '请输入相关关键字';
-        			var searchObj = $("#search_key");
-        			searchObj.val(default_key);
-        			searchObj.bind("focus",function(){
-        			
-        				if(searchObj.val() == default_key){
-        					searchObj.val('');
-        				}
-        			})
-        			
-        			searchObj.bind("blur",function(){
-        				if(searchObj.val() == ''){
-        					searchObj.val(default_key);
-        				}
-        			})
-        			
-        		})
+					// $(function(){
+        			// var default_key = '请输入相关关键字';
+        			// var searchObj = $("#search_key");
+        			// searchObj.val(default_key);
+        			// searchObj.bind("focus",function(){
+//         			
+        				// if(searchObj.val() == default_key){
+        					// searchObj.val('');
+        				// }
+        			// })
+//         			
+        			// searchObj.bind("blur",function(){
+        				// if(searchObj.val() == ''){
+        					// searchObj.val(default_key);
+        				// }
+        			// })
+//         			
+        		// })
 				
 
 
