1ed3edd615cbbf977f5b8e9f752c66defe45688c
yudaxue <yudaxue@163.com>
2014-06-21 16:13:02 +0800

    签证的关于我们，知识宝鼎

--- Home/Lib/Action/AboutAction.class.php
+++ Home/Lib/Action/AboutAction.class.php
@@ -15,19 +15,32 @@ class AboutAction extends Action{
 	//关于我们;
 	public  function aboutUs(){
 		$this->getPage('关于我们');
-		$this->display();
+		$this->display('Qzindex:aboutUs');
 	}
 	
+	//速格动态
+	public  function sgdt(){
+		$this->getPage('速格动态');
+		$this->display('Qzindex:sgdt');
+	}
 	
 	//我们的优势
 	public function wmdys(){
 		$this->getPage('我们的优势');
-		$this->display();		
+		$this->display('Qzindex:wmdys');		
 	}
 	
 	
 	public function getPage($title){
-		$alone = M('alonepage');
+		//判断登录
+		if(!session('FEUSER') || session('FEUSER')==''){
+			$iflogin = 0;
+		}else{
+			$iflogin = 1;
+			$this->assign('user_info',session('FEUSER'));
+		}
+		$this->assign('iflogin',$iflogin);
+		$alone = M('inte.alonepage',' ');
 		$contactUs =$alone->where("pageName='{$title}'")->find();
 		if(!$contactUs) exit('数据未找到.!');
 		$this->assign('cxzz',$contactUs);
