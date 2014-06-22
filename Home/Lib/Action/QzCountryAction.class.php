37e3b745515e143f4d599a2a05e1a30100c14363
yudaxue <yudaxue@163.com>
2014-06-22 12:10:15 +0800

    签证的单证与使馆签证

--- Home/Lib/Action/QzCountryAction.class.php
+++ Home/Lib/Action/QzCountryAction.class.php
@@ -1,9 +1,20 @@
 <?php
 class QzCountryAction extends Action{
+	public function _initialize(){
+		//判断登录
+		if(!session('FEUSER') || session('FEUSER')==''){
+			$iflogin = 0;
+		}else{
+			$iflogin = 1;
+			$this->assign('user_info',session('FEUSER'));
+		}
+		$this->assign('iflogin',$iflogin);
+	}
 	//全部签证;
 	public function qzList(){
 		$qz = M('Category');
 		$name = $qz->where("pid=0")->select();
+		//循环里面查询，傻逼
 		foreach($name as $key=>$val){
 			$rows = $qz->where("pid={$val['id']}")->select();
 			$name[$key]['sublist']=$rows;
@@ -12,15 +23,15 @@ class QzCountryAction extends Action{
 		$this->display();
 	}
 	
-	//常见认证,单证认证,各国使馆认证
+	//常见签证,单证签证,各国使馆签证
 	public function qzClass(){
 		$qz = M('Category');
 		$name = $qz->where("pid=0")->select();
 		$id = $_GET['id'];
-			$m=$qz->where("pid={$id}")->select();
-			$this->assign('name',$name);
-			$this->assign('msg',$m);
-			$this->display();
+		$m=$qz->where("pid={$id}")->select();
+		$this->assign('name',$name);
+		$this->assign('msg',$m);
+		$this->display();
 	}
 }
 ?>
