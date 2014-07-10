1773180c7e2a5b04605f56e57299ca9404f63190
yudaxue <yudaxue@163.com>
2014-07-10 11:42:57 +0800

    帮助中心控制器继承类修改

--- Home/Lib/Action/HelpAction.class.php
+++ Home/Lib/Action/HelpAction.class.php
@@ -1,5 +1,5 @@
 <?php
-class HelpAction extends CommonAction{
+class HelpAction extends Action{
 	public function help(){
 		$id = $_GET['id']?$_GET['id']:1;
 		$content = M('Help_list')->where('id='.$id.' and status = 2')->find();
