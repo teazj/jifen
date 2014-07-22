2316eaaff087827502c945061f6f63f1dc599e9c
yudaxue <yudaxue@163.com>
2014-07-22 23:20:28 +0800

    两个问题的解决

--- Home/Lib/Action/AboutAction.class.php
+++ Home/Lib/Action/AboutAction.class.php
@@ -30,6 +30,24 @@ class AboutAction extends Action{
 		$this->display('Qzindex:wmdys');		
 	}
 	
+	//////////////////////////////////
+	//关于我们;
+	public  function rzaboutUs(){
+		$this->getPage('关于我们');
+		$this->display('Rzindex:aboutUs');
+	}
+	
+	//速格动态
+	public  function rzsgdt(){
+		$this->getPage('速格动态');
+		$this->display('Rzindex:sgdt');
+	}
+	
+	//我们的优势
+	public function rzwmdys(){
+		$this->getPage('我们的优势');
+		$this->display('Rzindex:wmdys');		
+	}
 	
 	public function getPage($title){
 		//判断登录
