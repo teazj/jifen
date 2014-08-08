f87e1e881110a06baf5929b777755804673726fa
yudaxue <yudaxue@163.com>
2014-08-08 15:55:16 +0800

    xiufu1

--- Home/Lib/Action/VipAction.class.php
+++ Home/Lib/Action/VipAction.class.php
@@ -28,7 +28,6 @@ class VipAction extends CommonAction{
 		if($m['point']<0){
 			$m['point']=0;
 		}
-		print_r($m);
 		$this->assign('v',$m);
 		$this->display('Vip_persion');
 	}
@@ -370,7 +369,8 @@ class VipAction extends CommonAction{
 		$m=M('Intelog')->where($condition)->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		$this->assign('m',$m);
 		$this->assign('page',$show);
-		$this->assign('point',$_SESSION['FEUSER']['point']);
+		$point = M('Users')->where("id=".$_SESSION['FEUSER']['id'])->getField('point');
+		$this->assign('point',$point);
 		$this->display('Vip_exchange');
 	}
 	
